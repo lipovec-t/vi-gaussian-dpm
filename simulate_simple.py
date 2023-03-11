338f796087a2a0e39ee852eafc9fdaeb6db5422a
Thomas Lipovec <63391717+lipovec-t@users.noreply.github.com>
2023-03-11 22:20:44 +0100

    Add estimators to post processing module

--- simulate_simple.py
+++ simulate_simple.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 # Local application imports
 from data.generation import generate_data
 from vi.cavi import coordinates_ascent
+from vi import postprocessing as pp
 from config_simple import Params
 
 # random seed for testing purposes
@@ -32,68 +33,50 @@ t_0 = timeit.default_timer()
 elbo_final, tau, gamma, phi = coordinates_ascent(data, params)
 # end timer
 
+# end timer and compute elapsed time
 t_1 = timeit.default_timer()
-# compute elapsed time
 runtime = t_1 - t_0
 
 # postprocessing
 
+# number of mixture componentes in the fitted model
+T = params.T
+
 # MAP estimate of the cluster assignements
-indicator_array_est = np.argmax(phi,axis=1)
+cluster_indicator_est = pp.est_clustering_map(phi)
+
+# MMSE estimate of the cluster weights
+cluster_weights_est = pp.est_cluster_weights_mmse(gamma)
+
+# MMSE estimate of the cluster means 
+cluster_means_est = pp.est_cluster_means_mmse(tau)
 
 # delete empty clusters
-cluster_indicators_est = np.unique(indicator_array_est)
-T_est = cluster_indicators_est.size
-
-# load parameters
-T = params.T
-K = params.K
-N = params.N
-# estimate of the cluster weights
-V = np.divide(gamma[:,0],np.sum(gamma,axis=1))
-pi = np.zeros(np.shape(V))
-pi[0] = V[0]
-for i in range (1,T):
-    temp = 1
-    for j in range (i):
-            temp = temp*(1-V[j])
-    pi[i] = V[i]*temp
-
-# MMSE estimate of the cluster means
-cluster_means_est = np.zeros((T,K))   
-cluster_means_est = tau[:,:-1]/np.repeat(tau[:,-1,np.newaxis], 1, axis=1)
-cluster_means_est = cluster_means_est[cluster_indicators_est, :]
+# cluster_indicators_est = np.unique(indicator_array_est)
+# T_est = cluster_indicators_est.size
 
 # Sample mean of the clusters
-cluster_average = np.zeros((T, K))
-counts = np.zeros(T)
-for i in range(N):
-    cluster_average[indicator_array_est[i],:] += data[i]
-    counts[indicator_array_est[i]] += 1
-cluster_average = cluster_average[cluster_indicators_est, :]
-counts = counts[cluster_indicators_est]
-counts = np.repeat(counts[:,np.newaxis], K, axis=1)
-cluster_average = np.divide(cluster_average, counts)
+cluster_averages = pp.cluster_sample_mean(data, cluster_indicator_est, T)
 
 # Plot
 # map estimated cluster indicators to a range from 0 to T_est-1 for plotting
-plot_cluster_indicators = np.arange(T_est)
-mapper = lambda i: np.where(cluster_indicators_est == i)[0]
-plot_indicator_array_est = list(map(mapper, indicator_array_est))
-# Scatter plot with MMSE mean of clusters
-plt.figure()
-plt.title("Clustering DPM - MMSE Mean")
-if T_est > 10:
-    print('More clusters than colors')
-colormap = plt.cm.get_cmap('tab20', 10)
-cx, cy = cluster_means_est[:,0], cluster_means_est[:,1]
-plt.scatter(cx, cy, c=colormap(plot_cluster_indicators), marker="o")
-da, dy = data[:,0], data[:,1]
-plt.scatter(da, dy, c=colormap(plot_indicator_array_est), marker='.')
-# Scatter plot with sample mean of clusters
-plt.figure()
-plt.title("Clustering DPM - Cluster Sample Mean") 
-cx, cy = cluster_average[:,0], cluster_average[:,1]   
-plt.scatter(cx, cy, c = colormap(plot_cluster_indicators), marker="o")
-da, dy = data[:,0], data[:,1]
-plt.scatter(da, dy, c=colormap(plot_indicator_array_est), marker='.')
+# plot_cluster_indicators = np.arange(T_est)
+# mapper = lambda i: np.where(cluster_indicators_est == i)[0]
+# plot_indicator_array_est = list(map(mapper, indicator_array_est))
+# # Scatter plot with MMSE mean of clusters
+# plt.figure()
+# plt.title("Clustering DPM - MMSE Mean")
+# if T_est > 10:
+#     print('More clusters than colors')
+# colormap = plt.cm.get_cmap('tab20', 10)
+# cx, cy = cluster_means_est[:,0], cluster_means_est[:,1]
+# plt.scatter(cx, cy, c=colormap(plot_cluster_indicators), marker="o")
+# da, dy = data[:,0], data[:,1]
+# plt.scatter(da, dy, c=colormap(plot_indicator_array_est), marker='.')
+# # Scatter plot with sample mean of clusters
+# plt.figure()
+# plt.title("Clustering DPM - Cluster Sample Mean") 
+# cx, cy = cluster_average[:,0], cluster_average[:,1]   
+# plt.scatter(cx, cy, c = colormap(plot_cluster_indicators), marker="o")
+# da, dy = data[:,0], data[:,1]
+# plt.scatter(da, dy, c=colormap(plot_indicator_array_est), marker='.')
--- vi/postprocessing.py
+++ vi/postprocessing.py
@@ -1,11 +1,99 @@
 # TODO - each of the following shall be represented by its own function
-# full postprocessing that includes everything below, can be called in sim file 
-# MAP estimator for cluster assignments
-# MMSE estimator for cluster assigments?
+# full postprocessing that includes everything below, can be called in sim file
+import numpy as np
+ 
+def est_clustering_map(phi):
+    """
+    MAP estimates of cluster assignments given soft assignments.
+
+    Parameters
+    ----------
+    phi : ndarray
+        NxT array of soft assignments.
+
+    Returns
+    -------
+    Estimated cluster indicator for each datapoint.
+
+    """
+    return np.argmax(phi,axis=1)
+
+def est_cluster_weights_mmse(gamma):
+    """
+    MMSE estimate of the cluster weights given variational parameter gamma.
+
+    Parameters
+    ----------
+    gamma : ndarray
+        Tx2 variational parameter gamma describing T beta distributions.
+
+    Returns
+    -------
+    Estimated cluster weight for each of the T clusters.
+
+    """
+    T = gamma.shape[0]
+    V = np.divide(gamma[:,0],np.sum(gamma,axis=1))
+    pi = np.zeros(np.shape(V))
+    pi[0] = V[0]
+    for i in range (1,T):
+        temp = 1
+        for j in range (i):
+                temp = temp*(1-V[j])
+        pi[i] = V[i]*temp
+    
 # MMSE estimate for cluster means
+def est_cluster_means_mmse(tau):
+    """
+    MMSE stimate of the cluster means given variational parameter tau.
+
+    Parameters
+    ----------
+    tau : ndarray
+        Tx(K+1) variational parameter describing T exp. fam. distributions.
+
+    Returns
+    -------
+    Estimated cluster mean for each of the T clusters.
+
+    """
+    cluster_means_est = tau[:,:-1]/np.repeat(tau[:,-1,np.newaxis], 1, axis=1)
+    return cluster_means_est
+
+def cluster_sample_mean(data, cluster_indicators, num_clusters):
+    """
+    Sample means of the clusters. Sample mean of empty clusters is set to NaN.
+    
+    Parameters
+    ----------
+    data : ndarray
+        NxK data array.
+    cluster_indicators : ndarray
+        Nx1 cluster indicators.
+
+    Returns
+    -------
+    None.
+
+    """
+    N = data.shape[0]
+    K = data.shape[1]
+    cluster_sample_mean = np.zeros((num_clusters, K))
+    counts = np.zeros(num_clusters)
+    for i in range(N):
+        cluster_sample_mean[cluster_indicators[i],:] += data[i]
+        counts[cluster_indicators[i]] += 1
+    counts = np.repeat(counts[:,np.newaxis], K, axis=1)
+    cluster_sample_mean = np.divide(cluster_sample_mean, counts,\
+                                    where = counts > 0)
+    cluster_sample_mean[counts == 0] = np.nan
+    return cluster_sample_mean
+
+# MMSE estimator for cluster assigments?
+
 # Sample mean of clustered data
 # MAP estimator for cluster means?
-# MMSE estimator for cluster weights
+
 # MAP estimator for cluster weights?
 # plot posterior (contour plot, only for K=2, maybe also include K=1)
 # plot estimated cluster assignments, cluster means and cluster weights
