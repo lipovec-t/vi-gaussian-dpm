9478fd6a657584d83c568628b5bca42c043deb6d
Thomas Lipovec <63391717+lipovec-t@users.noreply.github.com>
2023-03-11 23:48:04 +0100

    Add method to reduced results and TODO for full pp

--- simulate_simple.py
+++ simulate_simple.py
@@ -51,32 +51,32 @@ cluster_weights_est = pp.est_cluster_weights_mmse(gamma)
 # MMSE estimate of the cluster means 
 cluster_means_est = pp.est_cluster_means_mmse(tau)
 
-# delete empty clusters
-# cluster_indicators_est = np.unique(indicator_array_est)
-# T_est = cluster_indicators_est.size
-
 # Sample mean of the clusters
-cluster_averages = pp.cluster_sample_mean(data, cluster_indicator_est, T)
+cluster_sample_mean, cluster_sample_weight, _ = \
+    pp.cluster_sample_mean(data, cluster_indicator_est, T)
+
+# Reduce results
+results = (cluster_indicator_est, cluster_weights_est, cluster_means_est,\
+           cluster_sample_mean)
+results_reduced = pp.reduce_results(results)
 
 # Plot
-# map estimated cluster indicators to a range from 0 to T_est-1 for plotting
-# plot_cluster_indicators = np.arange(T_est)
-# mapper = lambda i: np.where(cluster_indicators_est == i)[0]
-# plot_indicator_array_est = list(map(mapper, indicator_array_est))
-# # Scatter plot with MMSE mean of clusters
-# plt.figure()
-# plt.title("Clustering DPM - MMSE Mean")
-# if T_est > 10:
-#     print('More clusters than colors')
-# colormap = plt.cm.get_cmap('tab20', 10)
-# cx, cy = cluster_means_est[:,0], cluster_means_est[:,1]
-# plt.scatter(cx, cy, c=colormap(plot_cluster_indicators), marker="o")
-# da, dy = data[:,0], data[:,1]
-# plt.scatter(da, dy, c=colormap(plot_indicator_array_est), marker='.')
-# # Scatter plot with sample mean of clusters
-# plt.figure()
-# plt.title("Clustering DPM - Cluster Sample Mean") 
-# cx, cy = cluster_average[:,0], cluster_average[:,1]   
-# plt.scatter(cx, cy, c = colormap(plot_cluster_indicators), marker="o")
-# da, dy = data[:,0], data[:,1]
-# plt.scatter(da, dy, c=colormap(plot_indicator_array_est), marker='.')
+T_est = results_reduced[1].size
+plot_cluster_indicators = np.arange(T_est)
+# Scatter plot with MMSE mean of clusters
+plt.figure()
+plt.title("Clustering DPM - MMSE Mean")
+if T_est > 10:
+    print('More clusters than colors')
+colormap = plt.cm.get_cmap('tab20', 10)
+cx, cy = results_reduced[2][:,0], results_reduced[2][:,1]
+plt.scatter(cx, cy, c=colormap(plot_cluster_indicators), marker="o")
+da, dy = data[:,0], data[:,1]
+plt.scatter(da, dy, c=colormap(results_reduced[0]), marker='.')
+# Scatter plot with sample mean of clusters
+plt.figure()
+plt.title("Clustering DPM - Cluster Sample Mean") 
+cx, cy = results_reduced[3][:,0], results_reduced[3][:,1]   
+plt.scatter(cx, cy, c = colormap(plot_cluster_indicators), marker="o")
+da, dy = data[:,0], data[:,1]
+plt.scatter(da, dy, c=colormap(results_reduced[0]), marker='.')
--- vi/postprocessing.py
+++ vi/postprocessing.py
@@ -41,6 +41,7 @@ def est_cluster_weights_mmse(gamma):
         for j in range (i):
                 temp = temp*(1-V[j])
         pi[i] = V[i]*temp
+    return pi
     
 # MMSE estimate for cluster means
 def est_cluster_means_mmse(tau):
@@ -84,17 +85,41 @@ def cluster_sample_mean(data, cluster_indicators, num_clusters):
         cluster_sample_mean[cluster_indicators[i],:] += data[i]
         counts[cluster_indicators[i]] += 1
     counts = np.repeat(counts[:,np.newaxis], K, axis=1)
+    cluster_sample_weights = np.divide(counts, N)
     cluster_sample_mean = np.divide(cluster_sample_mean, counts,\
                                     where = counts > 0)
     cluster_sample_mean[counts == 0] = np.nan
-    return cluster_sample_mean
+    return cluster_sample_mean, cluster_sample_weights, counts
+
+def reduce_results(results):
+    """
+    Deletes results for empty clusters.
+    Assumes that results[0] is given by the estimated cluster indicators.
+
+    Parameters
+    ----------
+    results : tuple
+        Tuple containing different results for T clusters.
+
+    Returns
+    -------
+    Tuple containing reduced results with clusters numbered from 0 to the 
+    number of non-empty clusters.
+
+    """
+    num_results = len(results)
+    indicators_unique = np.unique(results[0])
+    # map estimated cluster indicators to a range from 0 to T_est-1
+    mapper = lambda i: np.where(indicators_unique == i)[0]
+    cluster_indicator_est = np.array(list(map(mapper, results[0])))[:,0]
+    reduced = [results[i][indicators_unique] for i in range(1, num_results)]
+    reduced.insert(0, cluster_indicator_est)
+    return tuple(reduced)
 
 # MMSE estimator for cluster assigments?
-
-# Sample mean of clustered data
 # MAP estimator for cluster means?
-
 # MAP estimator for cluster weights?
+
 # plot posterior (contour plot, only for K=2, maybe also include K=1)
 # plot estimated cluster assignments, cluster means and cluster weights
 
