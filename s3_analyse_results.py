d5602249c8a50333484caa89b63a87b230199f76
Thomas Lipovec <63391717+lipovec-t@users.noreply.github.com>
2023-04-03 10:18:05 +0200

    Fix analyse results of scenario 3

--- s3_analyse_results.py
+++ s3_analyse_results.py
@@ -32,18 +32,19 @@ counter = 0
 cluster_mean_distances = np.zeros(MC_runs)
 accuracy_score = np.zeros(MC_runs)
 OSPA = np.zeros(MC_runs)
+rmse = np.zeros(MC_runs)
 
 for i in range(MC_runs):
     est_cluster_number[i] = sim_data[i]["Estimated Number of Clusters"]
     true_cluster_means = sim_data[i]["True Cluster Means"]
-    est_cluster_means = sim_data[i]["Reordered Estimated Cluster Means"]
+    est_cluster_means = sim_data[i]["MMSE Estimated Cluster Means"]
     true_indicators = sim_data[i]["True Cluster Indicators"]
-    est_indicators = sim_data[i]["Relabelled Estimated Cluster Indicators"]
-    mean_indicators = sim_data[i]["Mean Indicators"]
+    est_indicators = sim_data[i]["MMSE Estimated Cluster Indicators"]
+    mean_indicators = sim_data[i]["MMSE Mean Indicators"]
     
     accuracy_score[i] = pp.accuracy_score(true_indicators, est_indicators)
     
-    OSPA[i], cluster_mean_distances[i] = pp.OSPA(true_cluster_means,\
+    OSPA[i], cluster_mean_distances[i], rmse[i] = pp.OSPA(true_cluster_means,\
                                                  est_cluster_means,\
                                                  mean_indicators)
     
@@ -52,6 +53,7 @@ accuracy_clusters = np.mean(est_cluster_number == 8) * 100
 accuracy_means = np.mean(cluster_mean_distances)
 mean_accuracy_score = np.mean(accuracy_score)
 mean_OSPA = np.mean(OSPA)
+mean_RMSE = np.mean(rmse)
 
 
 print("\n")
@@ -61,4 +63,5 @@ print(f"Accuracy Estimated Cluster numbers = {accuracy_clusters}%")
 print(f"Average distance to real cluster means = {accuracy_means}")
 print(f"Average accuracy score = {mean_accuracy_score}")
 print(f"Average OSPA = {mean_OSPA}")
+print(f"Average RMSE with cutoff = {mean_RMSE}")
 
