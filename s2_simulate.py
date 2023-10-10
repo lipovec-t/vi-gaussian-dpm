Fix formula of posterior expected log partition

s2_simulate.py
@@ -23,7 +23,7 @@ alpha = params.alpha
 
 # set object count and number of MC runs
 N_array = np.arange(1,51)
-MC_runs = 50
+MC_runs = 100
 
 # Store MSE for each simulation run
 MSE_x = np.zeros((N_array.size, MC_runs))
@@ -84,10 +84,10 @@ sigma_v = params.sigma_V[0,0]
 sigma_g = params.sigma_G[0,0]
 MSE_1 = (sigma_v * (sigma_u + sigma_g)) / (sigma_g + sigma_u + sigma_v)
 label = r'$\mathrm{MSE}_{\mathrm{min}}^{(1)}$'
-plt.axhline(y=MSE_1, color='black', linestyle='-', label=label)
+plt.axhline(y=MSE_1, color='gray', linestyle='-', label=label)
 MSE_2 = (sigma_v * sigma_u) / (sigma_u + sigma_v)
 label = r'$\mathrm{MSE}_{\mathrm{min}}^{(2)}$'
-plt.axhline(y=MSE_2, color='gray', linestyle='-', label=label)
+plt.axhline(y=MSE_2, color='black', linestyle='-', label=label)
 
 plt.xlabel('Number of objects')
 plt.ylabel('Average MSE')

vi/expectations.py
@@ -12,8 +12,10 @@ def log_V(gamma):
 def eta_and_logparteta(tau, sigma, sigma_inv):
     # As the two expectations are always used together we have one function for both
     K = tau.shape[1] - 1
+    # computation of posterior expected natural parameter eta
     expec_eta = np.einsum('ij,kj->ki',sigma_inv,tau[:,:-1]) / tau[:,-1,np.newaxis]
-    expec_logparteta = 1/(2*tau[:,-1]**2) * (np.sum(tau[:,:-1]*tau[:,:-1], axis=1) + K*tau[:,-1])
+    # computation of posterior expected log partition
+    # temp = tau_1^T * sigma_inv * tau_1 vectorized over t
+    temp = np.sum(tau[:,:-1] * np.dot(tau[:,:-1], sigma_inv), axis=1) 
+    expec_logparteta = 1/(2*tau[:,-1]**2) * (temp + K*tau[:,-1])
     return expec_eta, expec_logparteta
-
-    