63562d0cd10e0bd06df77e46df9ecf246c34214a
Thomas Lipovec <63391717+lipovec-t@users.noreply.github.com>
2023-10-13 10:32:56 +0200

    Split up s1 simulation script

---
+++ s1_clustering.py
@@ -0,0 +1,61 @@
+# Standard library imports
+import os
+
+# Third party imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Local application imports
+from data.generation import generate_data
+from vi.cavi import coordinates_ascent
+from vi import postprocessing as pp
+from s1_config import Params
+
+# Create folder where results are saved
+os.makedirs('results', exist_ok=True)
+
+# load parameters
+params = Params()
+
+# plot settings
+os.environ["PATH"] += os.pathsep + '/Library/TeX/texbin'
+plot_params = {"text.usetex"         : True,
+               "font.family"         : "serif",
+               "font.size"           : "11",
+               'text.latex.preamble' : r"\usepackage{bm}"}
+plt.rcParams.update(plot_params)
+
+#%% CAVI - Clustering
+alphaArray = [0.5,1,5]
+seedArray = [101,255,255]
+
+for (alpha, seed) in zip(alphaArray, seedArray):
+    np.random.seed(seed)
+    params.alpha_DPM = alpha
+    params.alpha     = alpha
+    data_dict = generate_data(params)
+    data = data_dict["Noisy Datapoints"]
+    _, _, tau, gamma, phi = coordinates_ascent(data_dict, params)
+    
+    # Postprocessing
+    results, results_reduced =\
+        pp.full_postprocessing(data_dict, phi, gamma, tau, relabel=True)
+        
+    # Plot true clustering
+    title = "True Clustering"
+    indicatorArray = data_dict["True Cluster Indicators"]
+    meanArray = data_dict["True Cluster Means"]
+    pp.plot_clustering(data, title, indicatorArray, meanArray)
+    plt.savefig(f"results/true_clusters_alpha{alpha}".replace(".", "")+".pdf",\
+                format="pdf", bbox_inches="tight")
+    
+    # Plot estimated clustering
+    title = "Estimated Clustering"
+    indicatorArray = results_reduced["MMSE Estimated Cluster Indicators"]
+    meanArray = results_reduced["MMSE Estimated Cluster Means"]
+    meanIndicators = results_reduced["MMSE Mean Indicators"]
+    pp.plot_clustering(data, title, indicatorArray, meanArray,\
+                        meanIndicators=meanIndicators)
+    plt.savefig(f"results/est_clusters_alpha{alpha}".replace(".", "")+".pdf",\
+                format="pdf", bbox_inches="tight")
+
--- s1_convergence.py
+++ s1_convergence.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 # Local application imports
 from data.generation import generate_data
 from vi.cavi import coordinates_ascent
-from vi import postprocessing as pp
 from s1_config import Params
 
 # Create folder where results are saved
@@ -25,38 +24,7 @@ plot_params = {"text.usetex"         : True,
                'text.latex.preamble' : r"\usepackage{bm}"}
 plt.rcParams.update(plot_params)
 
-#%% CAVI 
-np.random.seed(101)
-alpha = 0.5
-params.alpha_DPM = alpha
-params.alpha     = alpha
-data_dict = generate_data(params)
-data = data_dict["Noisy Datapoints"]
-_, _, tau, gamma, phi = coordinates_ascent(data_dict, params)
-
-# Postprocessing
-results, results_reduced =\
-    pp.full_postprocessing(data_dict, phi, gamma, tau, relabel=True)
-    
-# Plot true clustering
-title = "True Clustering"
-indicatorArray = data_dict["True Cluster Indicators"]
-meanArray = data_dict["True Cluster Means"]
-pp.plot_clustering(data, title, indicatorArray, meanArray)
-plt.savefig(f"results/true_clusters_alpha{alpha}.pdf".replace(".", "", 1),\
-            format="pdf", bbox_inches="tight")
-
-# Plot estimated clustering
-title = "Estimated Clustering"
-indicatorArray = results_reduced["MMSE Estimated Cluster Indicators"]
-meanArray = results_reduced["MMSE Estimated Cluster Means"]
-meanIndicators = results_reduced["MMSE Mean Indicators"]
-pp.plot_clustering(data, title, indicatorArray, meanArray,\
-                    meanIndicators=meanIndicators)
-plt.savefig(f"results/est_clusters_alpha{alpha}.pdf".replace(".", "", 1),\
-            format="pdf", bbox_inches="tight")
-
-#%% CAVI - Compare ELBO
+#%% CAVI - Convergence
 np.random.seed(2322259932) 
 alpha = 5
 params.alpha_DPM = alpha
@@ -74,7 +42,6 @@ ax1.set_ylabel("Number of iterations")
 params.init_type = "uniform"
 elbo, elbo_converged_it[0], tau, gamma, phi = coordinates_ascent(data_dict, params)
 ax1.plot(np.trim_zeros(elbo, 'b'), color='b', label="Uniform")
-# ax1.plot(elbo_converged_it[0], elbo[elbo_converged_it[0]], marker='x')
 plt.axvline(x=elbo_converged_it[0], color='b', linestyle='--')
 
 params.init_type = "true"
